adb9ae1017c484fa782affef5f65101f3f8872d4
alezcf <ialeczander@gmail.com>
2024-07-22 22:13:34 -0400

    create format.logic for format date

---
+++ frontend/src/logic/format.logic.js
@@ -0,0 +1,19 @@
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses empiezan en 0
+    const year = date.getFullYear();
+    
+    return `${day}-${month}-${year}`;
+}
+
+export function formatTime(dateString) {
+    const date = new Date(dateString);
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // La hora '0' debe ser '12'
+    
+    return `${hours}:${minutes} ${ampm}`;
+}
