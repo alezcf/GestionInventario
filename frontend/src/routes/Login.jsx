bf7c734eb16dfaa757800bf28c5609e13599dbaf
alezcf <ialeczander@gmail.com>
2024-08-13 21:07:18 -0400

    create Spinner while load login screen

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import LoginForm from '../components/LoginForm';
 import { useNavigate } from 'react-router-dom';
 import '../css/Login.css';
+import '../css/Spinner.css'; // Importamos el CSS del spinner
+
 function Login() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true); // Estado para manejar la carga
+
+  useEffect(() => {
+    // Establecer un temporizador de 1.7 segundos
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1700);
+
+    // Limpiar el temporizador si el componente se desmonta antes de que se complete
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-screen">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
 
   if (localStorage.getItem('user')) {
     return (
       <>
-        <h2>Ya estas logeado!</h2>
+        <h2>¡Ya estás logeado!</h2>
         <button onClick={() => navigate('/')}>Ir a home</button>
       </>
     );
